e734f2c9d88223b7782535b4a863fd9c49830119
Jubai <55176076+heiyelidebaiyang@users.noreply.github.com>
2026-07-24 12:03:34 +0800

    Update links.js

--- js/links.js
+++ js/links.js
@@ -8,8 +8,8 @@ const navLinks = [
             { title: 'PDF工具箱2', desc: '拆分书籍对开页', icon: '📖', url: '../html/tools/PDF-tools2.html' },
             { title: 'PDF工具箱3', desc: '交替合并', icon: '🔢', url: '../html/tools/PDF-tools3.html' },
             { title: 'TinyPNG', desc: '图片在线压缩', icon: '🖼️', url: 'https://tinypng.com' },
-            { title: 'JSON格式化', desc: '在线校验与格式化', icon: '🔧', url: 'https://www.json.cn' }，
-            { title: '试卷净化工具', desc: '纯本地', icon: '🔧', url: '../html/tools/shijuan.html' }
+            { title: 'JSON格式化', desc: '在线校验与格式化', icon: '🔧', url: 'https://www.json.cn' },
+            { title: '试卷净化工具', desc: '纯本地', icon: '🔧', url: '../html/tools/shijuan.html' },
         ]
     },
     {
